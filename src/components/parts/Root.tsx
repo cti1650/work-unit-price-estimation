720892a9cdb5276195ec72ff237f40fe47785885
cti1650 <cti1650@gmail.com>
2021-09-06 00:34:51 +0900

    ✨ コンポーネントを自動計算できるように改良

--- src/components/parts/Root.tsx
+++ src/components/parts/Root.tsx
@@ -1,19 +1,33 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import type React from 'next';
+import { useCallback, useState } from 'react';
 
-const Root = () => {
+type RootType = {
+  basePrice: number,
+  baseCount: number,
+  defTime: number
+}
+
+const Root = (props: RootType) => {
+  const { basePrice, baseCount, defTime } = props;
+  const [time, setTime] = useState(defTime);
+  const handleChangeTime = useCallback((e) => {
+    setTime(e.target.value);
+  }, [time])
   return (
-    <div className="flex flex-col space-x-4 max-w-screen-sm">
-      <div className="font-extrabold bg-gray-500">
+    <div className="flex flex-col space-x-4 w-full">
+      <div className="font-extrabold bg-gray-300 py-4 px-8 rounded-xl shadow">
         <div className="flex justify-between">
           <div>作業：タイヤ交換</div>
-          <div>労務単価：1000円</div>
+          <div>労務単価：{basePrice}円</div>
         </div>
         <div className="flex justify-between">
           <div>作業時間</div>
           <div className="form-control">
             <input
               type="number"
+              value={time}
+              onChange={handleChangeTime}
               placeholder="作業時間(分)"
               className="input input-sm input-bordered"
             ></input>
@@ -26,16 +40,9 @@ const Root = () => {
         <div className="flex justify-between">
           <div className="max-w-md">
             <div className="flex justify-between items-center">
-              <div>年間作業時間（分）</div>
-              <div>
-                <input
-                  type="number"
-                  placeholder="作業時間(分)"
-                  className="input input-sm input-bordered"
-                ></input>
-              </div>
+              <div>年間作業時間（分）：{time * baseCount}</div>
             </div>
-            <div>年間想定額：2000円</div>
+            <div>年間想定額：{(time * baseCount) / 60 * basePrice}円</div>
           </div>
           <div className="">
             <button className="content-center p-10 btn btn-info">算出</button>
@@ -52,14 +59,23 @@ const Root = () => {
           </div>
         </div>
 
-        <div>
+        <div className='pt-2'>
           <ul>
+            <li className='flex flex-row'>
+              <p>関連リンク</p>
+              <p><input
+                type="text"
+                placeholder="関連リンク"
+                className="ml-4 w-full input input-sm input-info input-bordered"
+              />
+              </p>
+            </li>
             <li>
               関連リンク
               <input
                 type="text"
                 placeholder="関連リンク"
-                className="input input-sm input-info input-bordered"
+                className="ml-4 input input-sm input-info input-bordered"
               />
             </li>
             <li>
@@ -67,15 +83,7 @@ const Root = () => {
               <input
                 type="text"
                 placeholder="関連リンク"
-                className="input input-sm input-info input-bordered"
-              />
-            </li>
-            <li>
-              関連リンク
-              <input
-                type="text"
-                placeholder="関連リンク"
-                className="input input-sm input-info input-bordered"
+                className="ml-4 input input-sm input-info input-bordered"
               />
             </li>
           </ul>
@@ -84,5 +92,11 @@ const Root = () => {
     </div>
   );
 };
+
+Root.defaultProps = {
+  basePrice: 1000,
+  baseCount: 1,
+  defTime: 60
+}
 
 export default Root;
